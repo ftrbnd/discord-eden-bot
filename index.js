34262f62fe8f6007441270ac59fde17e6ba03f69
Giovanni Salas <giosalas25@gmail.com>
2020-06-20 12:30:09 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+const bot = new Discord.Client();
+
+const token = process.env.DISCORD_TOKEN;
+const prefix = process.env.PREFIX;
+
+bot.on('ready', () => {
+    console.log('This bot is online!');
+});
+
+
+bot.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
+    if(!channel)
+        return;
+    
+    channel.send(`Welcome to the **futurebound** server, ${member}! \nIf you have a favorite album/EP, go to <#702231983853666335> and a color will be added to your name :)`)
+})
+
+bot.login();
