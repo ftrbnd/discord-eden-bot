c350729c256a72771b2ca9f6c8308af8840e91b4
Giovanni Salas <giosalas25@gmail.com>
2020-06-20 16:36:39 -0700

    final line fixes

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.on('message', message => {
             //const jon = bot.emojis.get("655848045287571456")
             message.channel.send(`Welcome to the **futurebound** Discord server! \nServer link: https://discord.link/futurebound 
             \n **Rules:** \n> ■ Do not spam \n > ■ No derogatory slurs/terms \n > ■ Be aware of the text channel topics \n > ■ Be considerate of others in voice channels \n > ■ Use the appropriate text channels when in a voice channel \n > ■ No inappropriate nicknames \n > ■ \"*be kind and respectful uwu*\" - anna 
-            \nMessage a <@&691882703674540042> if you have any questions: \n <@617075082564730880> \n <@166755438707212289> \n <@190533083341127681> \n <@201917777185865729> \n <@240634156650856448> \n <@617075082564730880> `);
+            \nMessage a <@&691882703674540042> if you have any questions: \n <@617075082564730880> \n <@166755438707212289> \n <@190533083341127681> \n <@201917777185865729> \n <@326615547565441024> \n <@240634156650856448> `);
             break;
     }
 })
