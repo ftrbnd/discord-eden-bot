17e0c243cde3b41706d0250e5b483c015473b16d
Giovanni Salas <giosalas25@gmail.com>
2020-06-22 13:31:02 -0700

    readded grape

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ client.on('message', message => {
         else if(message.content.includes('love')) {
             message.channel.send(`${message.author} i love you too x`);
         }
-        // else if(message.content.includes('grape')) {
-        //     message.channel.send(`${message.author} shut up you grape lookin slut`);
-        // }
+        else if(message.content.includes('grape')) {
+            message.channel.send('🍇');
+        }
         else if(message.content.includes('sex') || message.content.includes('catching feelings')) {
             message.channel.send(`catching feelings > sex`);
         }
