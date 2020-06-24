bb55f682f356f50872b32730eb7aba1ebb8c92d8
Giovanni Salas <giosalas25@gmail.com>
2020-06-24 15:30:49 -0700

    fixes

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ client.on('guildMemberAdd', member => {
     channel.send(`Welcome to the **futurebound** server, ${member}! \nIf you have a favorite album/EP, go to <#702231983853666335> and a color will be added to your name :)`)
 });
 
-client.on('message', message => {
+client.on('message', async message => {
     let args = message.content.substring(prefix.length).split(" "); // if prefix is used
     const serverQueue = queue.get(message.guild.id);
 
