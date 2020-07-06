640959b2e4d3aefeac6cdff13f37109a0718a95f
Giovanni Salas <giosalas25@gmail.com>
2020-07-06 02:02:21 -0700

    assigned announcement channel

--- src/commands/moderators/announce.js
+++ src/commands/moderators/announce.js
@@ -2,7 +2,7 @@ module.exports.run = async(client, message, args) => {
     if(message.member.roles.cache.has('691882703674540042')) { // Moderator
         let announcement = message.content.substring(10); //length of command 'announce' + 2
 
-        const targetChannel = 'test-commands'
+        const targetChannel = 'announcements'
         let announcingChannel = client.channels.cache.find(channel => channel.name.toLowerCase() === targetChannel)
         if(announcingChannel) {
             announcingChannel.send(announcement);
