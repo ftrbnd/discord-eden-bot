308544bb0c3b8303ce3028b4f1cc79b20bc9cb31
Giovanni Salas <giosalas25@gmail.com>
2020-07-12 00:44:27 -0700

    now detects server boosts through message type instead of the guildMemberUpdate event

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -1,30 +1,29 @@
-const Discord = require('discord.js');
-
 module.exports = (client, oldMember, newMember) => {
-    const channel = newMember.guild.channels.cache.find(channel => channel.name === "welcome");
+    const channel = newMember.guild.channels.cache.find(channel => channel.name === "test-commands");
     if(!channel)
         return;
     
     if(oldMember.roles.cache.size !== newMember.roles.cache.size) {
         // check if they didn't have SERVER BOOSTER role before and did have it after
-        if(!oldMember.roles.cache.has('704966097434312766') && newMember.roles.cache.has('704966097434312766')) {
-            newMember.roles.add('731688468366884934'); // 'futurebound' role
+        // if(!oldMember.roles.cache.has('704966097434312766') && newMember.roles.cache.has('704966097434312766')) {
+        //     newMember.roles.add('731688468366884934'); // 'futurebound' role
 
-            const boostEmoji = client.emojis.cache.get('731696862460903534');
-            const embed = new Discord.MessageEmbed()
-                .setAuthor(newMember.displayName + ' just boosted the server!', newMember.user.displayAvatarURL())
-                .setColor(0xf47fff)
-                .setThumbnail('https://emoji.gg/assets/emoji/1819_boostingtop.gif')
-                .setDescription(`They are now <@&731688468366884934>.`)
-                .setFooter('futurebound', 'https://i.imgur.com/8TsEfzo.jpg')
-                .setTimestamp();
+        //     const embed = new Discord.MessageEmbed()
+        //         .setAuthor(newMember.displayName + ' just boosted the server!', newMember.user.displayAvatarURL())
+        //         .setColor(0xf47fff)
+        //         .setThumbnail('https://emoji.gg/assets/emoji/1819_boostingtop.gif')
+        //         .setDescription(`They are now <@&731688468366884934>.`)
+        //         .setFooter('futurebound', 'https://i.imgur.com/8TsEfzo.jpg')
+        //         .setTimestamp();
             
-            channel.send(`${newMember}`);
-            channel.send(embed);
-        }
+        //     channel.send(`${newMember}`);
+        //     channel.send(embed);
+        // }
         // else if the SERVER BOOSTER role is gone
-        else if(oldMember.roles.cache.has('704966097434312766') && !newMember.roles.cache.has('704966097434312766')) {
-            newMember.roles.remove('731688468366884934'); // 'futurebound' role
+        // else
+        if(oldMember.roles.cache.has('704966097434312766') && !newMember.roles.cache.has('704966097434312766')) {
+            newMember.roles.remove('731688468366884934'); // remove 'futurebound' role
+            channel.send(`${newMember}'s server boost has left the server.`)
         }
     }
 }
--- src/events/message.js
+++ src/events/message.js
@@ -1,13 +1,68 @@
+const Discord = require('discord.js');
 const PREFIX = process.env.PREFIX;
 
-module.exports = (client, message) => {
+module.exports = async (client, message,) => {
     if(message.author.bot) return; // ignore if it's a bot
 
-    if(message.mentions.has(client.user) && !message.author.bot) { // if the bot is mentioned
+    const welcomeChannel = message.member.guild.channels.cache.find(channel => channel.name === "welcome");
+    if(!welcomeChannel)
+        return;
+
+    const embed = new Discord.MessageEmbed()
+    .setAuthor(message.member.displayName + ' just boosted the server!', message.member.user.displayAvatarURL()) // message + their avatar
+    .setColor(0xf47fff) // pink boost color
+    .setThumbnail('https://emoji.gg/assets/emoji/1819_boostingtop.gif') // nitro boost gif
+    .setFooter('futurebound', 'https://i.imgur.com/8TsEfzo.jpg') // server icon
+    .setTimestamp(); // when the boost happened
+
+    if(message.type === 'USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3') {
+        if(!message.member.roles.cache.has('731688468366884934'))  { // if they don't have the futurebound role
+            message.member.roles.add('731688468366884934'); // add futurebound role
+            embed.setDescription(`They are now <@&731688468366884934>. \n**futurebound** has achieved **Level 3**!`);
+        }
+        else {
+            embed.setDescription(`futurebound has achieved **Level 3**!`);
+        }
+        welcomeChannel.send(`${message.author}`); //message.author = User type
+        welcomeChannel.send(embed);
+    }
+    else if (message.type === 'USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2') {
+        if(!message.member.roles.cache.has('731688468366884934'))  { // if they don't have the futurebound role
+            message.member.roles.add('731688468366884934'); // add futurebound role
+            embed.setDescription(`They are now <@&731688468366884934>. \n**futurebound** has achieved **Level 2**!`);
+        }
+        else {
+            embed.setDescription(`futurebound has achieved **Level 2**!`);
+        }
+        welcomeChannel.send(`${message.author}`);
+        welcomeChannel.send(embed);
+    }
+    else if (message.type === 'USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_1') {
+        if(!message.member.roles.cache.has('731688468366884934'))  { // if they don't have the futurebound role
+            message.member.roles.add('731688468366884934'); // add futurebound role
+            embed.setDescription(`They are now <@&731688468366884934>. \n**futurebound** has achieved **Level 1**!`);
+        }
+        else {
+            embed.setDescription(`futurebound has achieved **Level 1**!`);
+        }
+        welcomeChannel.send(`${message.author}`);
+        welcomeChannel.send(embed);
+    }
+    else if (message.type === 'USER_PREMIUM_GUILD_SUBSCRIPTION') {
+        if(!message.member.roles.cache.has('731688468366884934'))  { // if they don't have the futurebound role
+            message.member.roles.add('731688468366884934'); // add futurebound role
+            embed.setDescription(`They are now <@&731688468366884934>.`);
+        }
+        welcomeChannel.send(`${message.author}`);
+        welcomeChannel.send(embed);
+    }
+
+
+    if(message.mentions.has(client.user) && !message.author.bot) { // if the bot is mentioned and it's not by a bot
         client.commands.get('mentions').run(client, message); 
     }
 
-    if(!message.content.startsWith(PREFIX)) return;
+    if(!message.content.startsWith(PREFIX)) return; // commands
 
     let cmdArgs = message.content.substring(message.content.indexOf(PREFIX)+1).split(new RegExp(/\s+/));
     let cmdName = cmdArgs.shift();
