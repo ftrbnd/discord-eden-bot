9e38af25a92c9b430bc855803f9f11c23aff1982
Giovanni Salas <giosalas25@gmail.com>
2020-07-13 09:31:21 -0700

    suggestions are sent every 5 upvotes

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -5,9 +5,9 @@ module.exports = async (client, reaction, user) => {
     if(!modChannel)
         return;
 
-    if (reaction.message.channel.name === 'suggestions' && reaction.emoji.name == '👍' && reaction.count >= 5) {
+    if (reaction.message.channel.name === 'suggestions' && reaction.emoji.name == '👍' && reaction.count % 5 === 0) {
         const embed = new Discord.MessageEmbed()
-            .setAuthor(reaction.message.member.displayName + `'s suggestion received ${reaction.count} thumbs up`, reaction.message.member.user.displayAvatarURL()) // message + their avatar
+            .setAuthor(reaction.message.member.displayName + `'s suggestion received ${reaction.count} upvotes`, reaction.message.member.user.displayAvatarURL()) // message + their avatar
             .setColor(0xff7818) // orange
             .setDescription(reaction.message.content)
             .setThumbnail(reaction.message.member.user.displayAvatarURL()) 
