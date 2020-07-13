35820198380da2903c7454ae08c7d7c26ba7572c
Giovanni Salas <giosalas25@gmail.com>
2020-07-13 11:58:11 -0700

    ?

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,22 +1,19 @@
 const {MessageEmbed} = require('discord.js');
 
 module.exports = async (client, reaction, user) => {
+    reaction.message.channel.send("hello?");
     const modChannel = reaction.message.member.guild.channels.cache.find(channel => channel.name === 'moderators');
-    console.log('hello1');
-    
-    try {
-        if(reaction.message.channel.name === 'suggestions' && reaction.emoji.name === '👍' && reaction.count % 10 === 0) {
-            console.log('hello2');
-            const embed = new MessageEmbed()
-                .setAuthor(reaction.message.member.displayName + `'s suggestion has received ${reaction.count} upvotes`, reaction.message.member.user.displayAvatarURL()) // message + their avatar
-                .setColor(0xff7818) // orange
-                .setDescription(reaction.message.content)
-                .setThumbnail(reaction.message.member.user.displayAvatarURL()) 
-                .setFooter('futurebound', 'https://i.imgur.com/8TsEfzo.jpg') // server icon
-                .setTimestamp(); // when 
-            modChannel.send(embed);
-        }
-    } catch(err) {
-        console.log(err);
+    if(!modChannel)
+        return;
+
+    if(reaction.message.channel.name === 'suggestions' && reaction.emoji.name === '👍' && reaction.count % 10 === 0) {
+        const embed = new MessageEmbed()
+            .setAuthor(reaction.message.member.displayName + `'s suggestion has received ${reaction.count} upvotes`, reaction.message.member.user.displayAvatarURL()) // message + their avatar
+            .setColor(0xff7818) // orange
+            .setDescription(reaction.message.content)
+            .setThumbnail(reaction.message.member.user.displayAvatarURL()) 
+            .setFooter('futurebound', 'https://i.imgur.com/8TsEfzo.jpg') // server icon
+            .setTimestamp(); // when 
+        modChannel.send(embed);
     }
 }
