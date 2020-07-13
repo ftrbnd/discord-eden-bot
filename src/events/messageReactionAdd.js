e6e7452a87369bed79ed55c0cf93d621db8d387e
Giovanni Salas <giosalas25@gmail.com>
2020-07-13 11:28:28 -0700

    fix

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,11 +1,11 @@
 const {MessageEmbed} = require('discord.js');
 
 module.exports = async (client, reaction, user) => {
-    const modChannel = reaction.message.member.guild.channels.cache.find(channel => channel.name === "moderators");
+    const modChannel = reaction.message.member.guild.channels.cache.find(channel => channel.name === 'moderators');
     if(!modChannel)
         return;
     console.log('hello1');
-    if(reaction.message.channel.name === 'suggestions' && reaction.emoji.name == '👍' && reaction.count % 10 == 0) {
+    if(reaction.message.channel.name === 'suggestions' && reaction.emoji.name === '👍' && reaction.count % 10 === 0) {
         console.log('hello2');
         const embed = new MessageEmbed()
             .setAuthor(reaction.message.member.displayName + `'s suggestion has received ${reaction.count} upvotes`, reaction.message.member.user.displayAvatarURL()) // message + their avatar
