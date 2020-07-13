6d4e51c145399646f275e2417cb24ac99a51080e
Giovanni Salas <giosalas25@gmail.com>
2020-07-13 01:27:05 -0700

    spelling

--- src/commands/music/pause.js
+++ src/commands/music/pause.js
@@ -1,6 +1,6 @@
 module.exports.run = async(client, message, args) => {
     if(message.member.roles.cache.has('691882703674540042')) { // Moderator
-        message.channel.send('music play is currently a work in progress.');
+        message.channel.send('music player is currently a work in progress.');
     }
     else {
         message.channel.send('music player is currently a work in progress.');
