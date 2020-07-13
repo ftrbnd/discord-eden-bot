05edd8dbeba98296c007f454c385bad03d4c9657
Giovanni Salas <giosalas25@gmail.com>
2020-07-13 10:32:40 -0700

    fixed?

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -4,7 +4,7 @@ module.exports = async (client, reaction, user) => {
     const modChannel = reaction.message.member.guild.channels.cache.find(channel => channel.name === "moderators");
     if(!modChannel)
         return;
-
+    
     if (reaction.message.channel.name === 'suggestions' && reaction.emoji.name == '👍' && reaction.count % 10 === 0) {
         const embed = new MessageEmbed()
             .setAuthor(reaction.message.member.displayName + `'s suggestion has received ${reaction.count} upvotes`, reaction.message.member.user.displayAvatarURL()) // message + their avatar
