68609eef546ecb8692b6cea92a0787904a384891
Giovanni Salas <giosalas25@gmail.com>
2020-07-13 11:32:14 -0700

    try catch

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -5,15 +5,20 @@ module.exports = async (client, reaction, user) => {
     if(!modChannel)
         return;
     console.log('hello1');
-    if(reaction.message.channel.name === 'suggestions' && reaction.emoji.name === '👍' && reaction.count % 10 === 0) {
-        console.log('hello2');
-        const embed = new MessageEmbed()
-            .setAuthor(reaction.message.member.displayName + `'s suggestion has received ${reaction.count} upvotes`, reaction.message.member.user.displayAvatarURL()) // message + their avatar
-            .setColor(0xff7818) // orange
-            .setDescription(reaction.message.content)
-            .setThumbnail(reaction.message.member.user.displayAvatarURL()) 
-            .setFooter('futurebound', 'https://i.imgur.com/8TsEfzo.jpg') // server icon
-            .setTimestamp(); // when 
-        modChannel.send(embed);
+    
+    try {
+        if(reaction.message.channel.name === 'suggestions' && reaction.emoji.name === '👍' && reaction.count % 10 === 0) {
+            console.log('hello2');
+            const embed = new MessageEmbed()
+                .setAuthor(reaction.message.member.displayName + `'s suggestion has received ${reaction.count} upvotes`, reaction.message.member.user.displayAvatarURL()) // message + their avatar
+                .setColor(0xff7818) // orange
+                .setDescription(reaction.message.content)
+                .setThumbnail(reaction.message.member.user.displayAvatarURL()) 
+                .setFooter('futurebound', 'https://i.imgur.com/8TsEfzo.jpg') // server icon
+                .setTimestamp(); // when 
+            modChannel.send(embed);
+        }
+    } catch(err) {
+        console.log(err);
     }
 }
