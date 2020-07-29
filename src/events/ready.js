7bfaec8ee0029a1bc8df6d159d94a1179b7b7a8b
Giovanni Salas <giosalas25@gmail.com>
2020-07-29 11:47:56 -0700

    added auto-changing status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,15 @@
 const {MessageEmbed} = require('discord.js');
 
 module.exports = (client) => {
-    client.user.setActivity(`New World Tapes`, {
-        type: "LISTENING",
-        url: "https://open.spotify.com/album/7HDfpCVCVM5nlR5blLX3E8"
-    });
+    function randomStatus() {
+        let status = ["no future", "New World Tapes"];
+        let randomIndex = Math.floor(Math.random() * status.length)
+        
+        client.user.setActivity(status[randomIndex], {
+            type: "LISTENING",
+            url: "https://open.spotify.com/album/7HDfpCVCVM5nlR5blLX3E8"
+        });
+    }; setInterval(randomStatus, 3600000)
 
     let announcingChannel = client.channels.cache.find(channel => channel.name.toLowerCase() === 'test-commands')
     if(announcingChannel) {
