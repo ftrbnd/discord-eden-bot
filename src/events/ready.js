a1c10aa7b9130048f09a893488d03ac306a97410
Giovanni Salas <giosalas25@gmail.com>
2020-07-29 12:09:03 -0700

    removed changing status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,15 +1,10 @@
 const {MessageEmbed} = require('discord.js');
 
 module.exports = (client) => {
-    function randomStatus() {
-        let status = ["no future", "New World Tapes"];
-        let randomIndex = Math.floor(Math.random() * status.length)
-        
-        client.user.setActivity(status[randomIndex], {
-            type: "LISTENING",
-            url: "https://open.spotify.com/album/7HDfpCVCVM5nlR5blLX3E8"
-        });
-    }; setInterval(randomStatus, 3600000)
+    client.user.setActivity(`New World Tapes`, {
+        type: "LISTENING",
+        url: "https://open.spotify.com/album/7HDfpCVCVM5nlR5blLX3E8"
+    });
 
     let announcingChannel = client.channels.cache.find(channel => channel.name.toLowerCase() === 'test-commands')
     if(announcingChannel) {
