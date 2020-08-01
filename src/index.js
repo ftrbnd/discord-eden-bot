f04de6360113ba9fbe9bcd98b17391b5733cdaf1
Giovanni Salas <giosalas25@gmail.com>
2020-08-01 15:10:49 -0700

    added twitter feed for @iameden

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
+const Twitter = require('twit');
+
 const fs = require('fs').promises;
 const path = require('path');
+const config = require('./config.js');
 
 const TOKEN = process.env.DISCORD_TOKEN;
 const PREFIX = process.env.PREFIX;
+const consumer_key = process.env.CONSUMER_KEY;
+const consumer_secret = process.env.CONSUMER_SECRET;
+const access_token = process.env.ACCESS_TOKEN;
+const access_token_secret = process.env.ACCESS_TOKEN_SECRET;
 
 const client = new Discord.Client();
 client.login(TOKEN);
 client.commands = new Map();
+
+const twitterClient = new Twitter(config);
+function isReply(tweet) {
+    if (tweet.retweeted_status || tweet.in_reply_to_status_id || tweet.in_reply_to_status_id_str || tweet.in_reply_to_user_id
+    || tweet.in_reply_to_user_id_str || tweet.in_reply_to_screen_name) 
+        return true;
+    return false;
+}
+const stream = twitterClient.stream('statuses/filter', {
+    follow: '1598790960', // twtter used id for @iameden
+});
+stream.on('tweet', tweet => {
+    const twitterMessage = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+    if(isReply(tweet) == false) {
+        const tweetText = tweet.text;
+        const tweetChannel = client.channels.cache.get('739243205807308831');
+        tweetChannel.send(twitterMessage);
+    }
+    return false;
+});
 
 // IIFE - immediately invoked function expression
 (async function registerCommands(dir = 'commands') {
