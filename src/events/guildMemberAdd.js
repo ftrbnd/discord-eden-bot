7a25d2698fed8931cf5c59096054c3d8e87a4869
Giovanni Salas <giosalas25@gmail.com>
2020-08-05 15:27:05 -0700

    test

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -13,6 +13,6 @@ module.exports = async (client, member) => {
         .setFooter('futurebound', 'https://i.imgur.com/8TsEfzo.jpg')
         .setTimestamp();
     
-    //channel.send(`${member}`);
+    console.log(channel.name);
     channel.send(`${member}`, embed);
 }
