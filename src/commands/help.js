841315da46b6171c445ce62edf61cc173ea47b8a
Giovanni Salas <giosalas25@gmail.com>
2020-08-27 00:08:24 -0700

    hello?

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,7 +11,7 @@ module.exports.run = async(client, message, args) => {
             {
                 name: 'Prefix',
                 value: process.env.PREFIX,
-                inline: true,
+                inline: false,
             },
             {
                 name: 'Commands',
